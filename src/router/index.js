87b846242d0dbf8cecf2ab08fb2ff4987485296a
JSkahl <josekahl333@gmail.com>
2025-04-23 09:18:51 -0300

    build: add vue-router

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // Routes go here, between {}
+  ]
+})
+
+export default router
