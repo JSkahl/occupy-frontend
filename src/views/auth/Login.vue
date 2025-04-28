6924bd547ae09c65356e11cccf71a0dd9e5b7748
JSkahl <josekahl333@gmail.com>
2025-04-28 19:36:33 -0300

    feat: create login screen

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Banner from "@/components/auth/common/Banner.vue";
+import LoginForm from "@/components/auth/login/LoginForm.vue";
+</script>
+
+<template>
+  <div class="container">
+    <Banner />
+    <LoginForm />
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    display: flex;
+  }
+</style>
