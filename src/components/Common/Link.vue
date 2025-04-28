8e3c669bc1c28eb4435e5ae247b397607b335f45
JSkahl <josekahl333@gmail.com>
2025-04-28 17:27:53 -0300

    feat: create link component

---
+++ src/components/Common/Link.vue
@@ -0,0 +1,24 @@
+<script setup>
+  defineProps({
+    link: {
+      type: String,
+      required: true,
+    },
+
+    text: {
+      type: String,
+      required: true,
+    }
+  })
+</script>
+
+<template>
+  <RouterLink :to="link" class="link">{{ text }}</RouterLink> 
+</template>
+
+<style scoped>
+.link {
+  text-decoration: none;
+  color: var(--blue);
+}
+</style>
