5d2b093a4495813f012a354a7700cd807dfb0c64
JSkahl <josekahl333@gmail.com>
2025-05-09 18:45:19 -0300

    feat: create personal register view

---
+++ src/views/auth/PersonalRegister.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Banner from "@/components/auth/common/Banner.vue";
+import PersonalRegisterForm from "@/components/auth/register/personal/PersonalRegisterForm.vue";
+</script>
+
+<template>
+  <div class="flex">
+    <Banner />
+    <PersonalRegisterForm />
+  </div>
+</template>
