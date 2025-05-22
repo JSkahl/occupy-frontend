830c641c1996bfa4e10f4045cb2581d0f8124509
JSkahl <josekahl333@gmail.com>
2025-05-22 19:12:28 -0300

    feat: create enterprise register form

---
+++ src/components/auth/register/enterprise/EnterpriseRegisterForm.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+
+import RegisterPageController from "@/components/auth/common/RegisterPageController.vue";
+import Title from "@/components/common/Title.vue";
+import EnterpriseIdInfo from "./EnterpriseIdInfo.vue";
+import LocaleInfo from "./LocaleInfo.vue";
+import ContactInfo from "./ContactInfo.vue";
+
+let counter = ref(0);
+</script>
+
+<template>
+  <div class="w-[50vw] h-screen flex flex-col justify-between">
+    <Title text="Cadastro empresarial" :size="1" class="pl-6 pt-6" />
+
+    <!--Pages-->
+    <EnterpriseIdInfo v-if="counter === 0"/>
+    <LocaleInfo v-if="counter === 1"/>
+    <ContactInfo v-if="counter === 2"/>
+
+    <!--Page controller-->
+    <RegisterPageController :dots="3" v-model:counter="counter" />
+  </div>
+</template>
