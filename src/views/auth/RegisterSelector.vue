cb8d73265f7db3f68ea7aab3a08969d3fe08a7d8
JSkahl <josekahl333@gmail.com>
2025-05-22 20:19:48 -0300

    feat: create register selector view

---
+++ src/views/auth/RegisterSelector.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Banner, RegisterSelectionForm } from "@/components"
+</script>
+
+<template>
+  <div class="flex">
+    <Banner />
+    <RegisterSelectionForm />
+  </div>
+</template>
