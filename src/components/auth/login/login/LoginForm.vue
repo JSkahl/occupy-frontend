b05698bf31718113d5ba1bdbed06cf53d44d7c4b
JSkahl <josekahl333@gmail.com>
2025-05-22 20:24:01 -0300

    chore: add routes to login form

--- src/components/auth/login/login/LoginForm.vue
+++ src/components/auth/login/login/LoginForm.vue
@@ -13,40 +13,24 @@ function funcTest() {
 </script>
 
 <template>
-  <div 
-    class="
-      w-[50vw] h-screen
-      flex flex-col justify-around items-center
-      py-[3%]
-    ">
+  <div class="w-[50vw] h-screen flex flex-col justify-around items-center py-[3%]">
     <Title text="Seja bem-vindo à Occupy!" :size="1" />
 
-    <div 
-      class="
-        w-full h-[40%]
-        flex flex-col items-center content-between
-    ">
-
-      <div 
-        class="
-          flex flex-col justify-around 
-          h-full
-        ">
+    <div class="w-full h-[40%] flex flex-col items-center content-between">
+      <div class="flex flex-col justify-around h-full">
         <Input label="Email" placeholder="Insira o seu email" :icon="Account" type="email" />
         <div>
-          <PasswordInput label="Senha" placeholder="Insira a sua senha"/>
+          <PasswordInput label="Senha" placeholder="Insira a sua senha" />
           <Link link="/" text="Esqueceu a sua senha?" :size="1" />
         </div>
       </div>
     </div>
 
-    <div 
-      class="
-        w-full h-[40%]
-        flex flex-col justify-evenly items-center
-      ">
+    <div class="w-full h-[40%] flex flex-col justify-evenly items-center">
       <Button variant="filled" text="Entrar" :onClick="funcTest" />
-      <Button variant="transparent" text="Cadastrar-se" :onClick="funcTest" />
+      <router-link to="/register">
+        <Button variant="transparent" text="Cadastrar-se" :onClick="funcTest" />
+      </router-link>
     </div>
   </div>
 </template>
