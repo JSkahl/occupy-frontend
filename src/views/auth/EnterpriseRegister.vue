9024cd0fb9677f490f3f011823fd297d3509be07
JSkahl <josekahl333@gmail.com>
2025-05-22 19:12:40 -0300

    feat: create enterprise register view

---
+++ src/views/auth/EnterpriseRegister.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Banner from "@/components/auth/common/Banner.vue";
+import EnterpriseRegisterForm from "@/components/auth/register/enterprise/EnterpriseRegisterForm.vue";
+</script>
+
+<template>
+  <div class="flex">
+    <Banner />
+    <EnterpriseRegisterForm />
+  </div>
+</template>
