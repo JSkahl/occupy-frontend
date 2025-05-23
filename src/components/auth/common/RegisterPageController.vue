24d3488cceb6751dad2a3a782d77a1c3c17968cf
JSkahl <josekahl333@gmail.com>
2025-05-23 16:38:00 -0300

    style: formatting code

--- src/components/auth/common/RegisterPageController.vue
+++ src/components/auth/common/RegisterPageController.vue
@@ -14,14 +14,16 @@ const props = defineProps({
   },
 });
 
-let counter = defineModel('counter')
+let counter = defineModel("counter");
 
 const decrement = () => {
   counter.value <= 0 ? (counter.value = 0) : counter.value--;
 };
 
 const increment = () => {
-  counter.value >= props.dots - 1 ? (counter.value = props.dots - 1) : counter.value++;
+  counter.value >= props.dots - 1
+    ? (counter.value = props.dots - 1)
+    : counter.value++;
 };
 </script>
 
@@ -29,36 +31,34 @@ const increment = () => {
   <div>
     <!--Buttons-->
     <div class="w-[50vw] flex px-2.5 justify-end">
-      
       <!--Left button-->
-      <div v-if="counter != 0"
+      <div
+        v-if="counter != 0"
         class="w-12 h-12 flex justify-center items-center bg-[var(--blue)] hover:bg-[var(--darker-blue)] text-[var(--white)] rounded-lg cursor-pointer"
-        @click="decrement()">
-
+        @click="decrement()"
+      >
         <ArrowLeft />
       </div>
-      
+
       <!--Spacer-->
       <div v-if="counter != 0" class="flex-grow"></div>
 
       <!--Right button-->
       <div
         class="w-12 h-12 flex justify-center items-center bg-[var(--blue)] hover:bg-[var(--darker-blue)] text-[var(--white)] rounded-lg cursor-pointer"
-        @click="increment()">
-
+        @click="increment()"
+      >
         <ArrowRight />
       </div>
     </div>
 
     <!--Dots-->
     <div class="w-[50vw] h-12 px-[40%] flex justify-between items-center">
-
-      <div 
-        v-for="dot in dots" 
+      <div
+        v-for="dot in dots"
         class="w-4 h-4 rounded-full shadow-[0_4px_3px_rgba(0,0,0,0.25)]"
-        :class="[counter === dot - 1 ? 'selected' : 'not-selected']">
-      </div>
-
+        :class="[counter === dot - 1 ? 'selected' : 'not-selected']"
+      ></div>
     </div>
   </div>
 </template>
