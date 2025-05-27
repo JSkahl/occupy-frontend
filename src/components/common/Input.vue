b4faf9113cc03877848e38256a55d2423247d9a5
JSkahl <josekahl333@gmail.com>
2025-05-27 18:37:55 -0300

    chore: fix input value read

--- src/components/common/Input.vue
+++ src/components/common/Input.vue
@@ -19,7 +19,14 @@ defineProps({
     type: String,
     required: true,
   },
+
+  modelValue: {
+    type: String,
+    required: false,
+  },
 });
+
+defineEmits(["update:modelValue"]);
 </script>
 
 <template>
@@ -27,10 +34,16 @@ defineProps({
     <label class="text-[var(--blue)] pl-[1%]">{{ label }}</label>
 
     <div
-      class="flex justify-around border border-[var(--blue)] text-[var(--gray)] placeholder-[var(--gray)] rounded-md gap-1 pr-1 pl-1 w-52 h-6 md:w-64 md:h-8 lg:w-72 lg:h-10 xl:w-xs xl:h-12">
+      class="flex justify-around border border-[var(--blue)] text-[var(--gray)] placeholder-[var(--gray)] rounded-md gap-1 pr-1 pl-1 w-52 h-6 md:w-64 md:h-8 lg:w-72 lg:h-10 xl:w-xs xl:h-12"
+    >
       <component :is="icon" />
 
-      <input :type="type" :placeholder="placeholder" class="border-none focus:outline-none w-full h-full" />
+      <input
+        @input="$emit('update:modelValue', $event.target.value)"
+        :value="modelValue"
+        :placeholder="placeholder"
+        class="border-none focus:outline-none w-full h-full"
+      />
     </div>
   </div>
 </template>
