afd60cac38df2fad5931747a7839d19e891dd47d
JSkahl <josekahl333@gmail.com>
2025-05-29 16:12:40 -0300

    chore: implement data gather in selector input

--- src/components/common/selector/Dropdown.vue
+++ src/components/common/selector/Dropdown.vue
@@ -1,34 +1,23 @@
 <script setup>
-import { ref } from 'vue';
-
-const empresas = ref([
-{ empresa: 'Empresa 1' },
-{ empresa: 'Empresa 2' },
-{ empresa: 'Empresa 3' },
-{ empresa: 'Empresa 4' },
-{ empresa: 'Empresa 5' },
-{ empresa: 'Empresa 6' },
-{ empresa: 'Empresa 7' },
-{ empresa: 'Empresa 8' },
-{ empresa: 'Empresa 9' },
-{ empresa: 'Empresa 10' },
-])
+const props = defineProps({
+  options: {
+    type: Array,
+    required: true,
+  },
+});
 </script>
 
 <template>
-  <div 
-    class="
-      bg-(--white) rounded-md shadow-[0px_10px_16px_7px_rgba(0,0,0,0.25)]
-      p-2 absolute
-      overflow-scroll
-      gap-[30px]
-      md:w-64 md:max-h-24
-      lg:w-72 lg:max-h-28
-      xl:w-xs xl:max-h-36
-    ">
-    <p v-for="x in empresas" 
-      class="
-        hover:bg-(--light-white)
-      ">{{ x.empresa}}</p>
+  <div
+    class="bg-(--white) rounded-md shadow-[0px_10px_16px_7px_rgba(0,0,0,0.25)] p-2 absolute overflow-scroll gap-[30px] md:w-64 md:max-h-24 lg:w-72 lg:max-h-28 xl:w-xs xl:max-h-36"
+  >
+    <option
+      v-for="option in options"
+      :key="option"
+      @click="$emit('select', option)"
+      class="hover:bg-(--light-white)"
+    >
+      {{ option }}
+    </option>
   </div>
 </template>
--- src/components/common/selector/Selector.vue
+++ src/components/common/selector/Selector.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed, watch } from "vue";
 
-import MenuDown from 'vue-material-design-icons/MenuDown.vue'
-import { Dropdown } from '@/components';
+import MenuDown from "vue-material-design-icons/MenuDown.vue";
+import { Dropdown } from "@/components";
 
-defineProps({
+const props = defineProps({
   label: {
     type: String,
     required: true,
@@ -18,50 +18,84 @@ defineProps({
   icon: {
     type: Object,
     required: false,
-  }
+  },
+
+  options: {
+    type: Array,
+    required: false,
+  },
 });
 
-let isDroped = ref(false)
+const emit = defineEmits(["update:modelValue", "error"]);
 
-const dropMenu = () => { isDroped.value = !isDroped.value }
+const inputValue = ref("");
+const isDroped = ref(false);
+const errorMessage = ref("");
+
+const filteredOptions = computed(() => {
+  if (!inputValue.value) return props.options;
+  return props.options.filter((option) =>
+    option.toLowerCase().includes(inputValue.value.toLowerCase()),
+  );
+});
+
+const handleSelect = (option) => {
+  inputValue.value = option;
+  isDroped.value = false;
+  emit("update:modelValue", option);
+  errorMessage.value = "";
+  isDroped.value = false;
+};
+
+function validateInput() {
+  isDroped.value = false;
+  const isValid = props.options.includes(inputValue.value);
+  if (!isValid) {
+    emit("error", "Invalid selection");
+    errorMessage.value = "Insira uma opção existente";
+  } 
+
+  return isValid;
+}
+
+watch(inputValue, (val) => {
+  emit("update:modelValue", val);
+});
+
+const dropMenu = () => {
+  isDroped.value = !isDroped.value;
+};
 </script>
 
 <template>
-<div>
-  <label class="
-    text-[var(--blue)] pl-[1%]
-  ">{{ label }}</label>
-  <div 
-    class="
-      flex justify-around
-      border border-(--blue) text-(--gray) placeholder-(--gray)
-      rounded-md gap-1
-      pr-1 pl-1
-      w-52 h-6
-      md:w-64 md:h-8
-      lg:w-72 lg:h-10
-      xl:w-xs xl:h-12
-    ">
+  <div>
+    <label class="text-[var(--blue)] pl-[1%]">{{ label }}</label>
+    <div
+      class="flex justify-around border border-(--blue) text-(--gray) placeholder-(--gray) rounded-md gap-1 pr-1 pl-1 w-52 h-6 md:w-64 md:h-8 lg:w-72 lg:h-10 xl:w-xs xl:h-12"
+    >
+      <component :is="icon" />
 
-    <component :is="icon" />
+      <input
+        type="text"
+        v-model="inputValue"
+        @focus="isDroped = !isDroped"
+        @blur="validateInput"
+        :placeholder="placeholder"
+        class="border-none focus:outline-none w-full h-full"
+      />
 
-    <input type="text" :placeholder="placeholder" 
-    class="
-      border-none focus:outline-none
-      w-full h-full
-    "/>
-  
-    <span @click="dropMenu()" 
-      class="
-        cursor-pointer
-        target:rotate-180
-      ">
-      <MenuDown/>
-    </span>
-      </div>
-  <Dropdown v-if="isDroped"/>
-
-</div>
+      <span @click="dropMenu()" class="cursor-pointer target:rotate-180">
+        <MenuDown />
+      </span>
+    </div>
+    <Dropdown
+      v-if="isDroped"
+      @select="handleSelect"
+      :options="filteredOptions"
+      class="z-50"
+    />
+    <p v-if="errorMessage" class="absolute text-red-500 text-sm">{{ errorMessage }}</p>
+  </div>
 </template>
 
 <style scoped>
