4af7e8f65a2e5004d881e9a52c7d5b1c8693bd7f
JSkahl <josekahl333@gmail.com>
2025-06-05 14:17:30 -0300

    chore: setting up axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,35 +1,36 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+	baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
+	(config) => {
+		const token = localStorage.getItem("token");
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	},
 );
 
 api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response.status === 401) {
-      console.log("Unauthorized");
-    }
-    if (error.response.status === 403) {
-      console.log("Forbbiden");
-    }
-    if (error.response.status === 404) {
-      console.log("Not found");
-    }
-  },
+	(response) => response,
+	(error) => {
+		if (error.response.status === 401) {
+			console.log("Unauthorized");
+		}
+		if (error.response.status === 403) {
+			console.log("Forbbiden");
+		}
+		if (error.response.status === 404) {
+			console.log("Not found");
+		}
+		return Promise.reject(error);
+	},
 );
 
 export default api;
