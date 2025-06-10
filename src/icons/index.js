409dcc5df8c2f3360478b5586aac0f669b47f548
JSkahl <josekahl333@gmail.com>
2025-06-10 20:33:51 -0300

    chore: import icons

---
+++ src/icons/index.js
@@ -0,0 +1,21 @@
+import Home from "vue-material-design-icons/Home.vue";
+import StoreSearch from "vue-material-design-icons/StoreSearch.vue";
+import Book from "vue-material-design-icons/Book.vue";
+import Autorenew from "vue-material-design-icons/Autorenew.vue";
+import Map from "vue-material-design-icons/Map.vue";
+import Send from "vue-material-design-icons/Send.vue";
+import HelpCircle from "vue-material-design-icons/HelpCircle.vue";
+import AccountCog from "vue-material-design-icons/AccountCog.vue";
+import MicrosoftOffice from "vue-material-design-icons/MicrosoftOffice.vue";
+
+export {
+    Home,
+    StoreSearch,
+    Book,
+    Autorenew,
+    Map,
+    Send,
+    HelpCircle,
+    AccountCog,
+    MicrosoftOffice,
+};
