eb6037f66d4548e29258d150623fffd2c92d3747
JSkahl <josekahl333@gmail.com>
2025-06-10 16:13:18 -0300

    feat: create aside and header components

---
+++ src/components/global/AsideMenu.vue
@@ -0,0 +1,9 @@
+<script setup>
+    
+</script>
+
+<template>
+    <aside>
+        <h1>Menu Aside!</h1>
+    </aside>
+</template>
---
+++ src/components/global/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Title } from '@/components'
+import Whatsapp from 'vue-material-design-icons/Whatsapp.vue'
+import AccountCircle from 'vue-material-design-icons/AccountCircle.vue'
+</script>
+
+<template>
+    <header class="bg-(--blue) text-(--white) w-[100vw] h-14 flex items-center justify-between p-2">
+        <Title text="OCCUPY" :size="3" />
+        <div class="flex w-[4%] justify-between">
+            <Whatsapp />
+            <AccountCircle />
+        </div>
+    </header>
+</template>
